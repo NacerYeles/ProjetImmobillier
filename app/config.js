85ae4e074a7b2f92ff204767b123b79bb6a8582c
nacer <nacer.yeles@gmail.com>
2021-04-28 16:40:58 +0200

    📄 update fichier config 📄

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     directory_product_image : __dirname+'/../public/images/products',
     appKey: 'cf0d9c3385323d6c70d38357a8aa9385a',
-    port : 3000
+    port : process.env.PORT || 3000
 };
